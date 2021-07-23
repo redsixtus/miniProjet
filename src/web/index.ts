6a7f39b2f1e18743037ee240a4126a0ab36715f4
Your Name <you@example.com>
2021-07-23 17:34:58 +0200

    create commit

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,15 @@
-alert("hello world")
+import {Service} from "../common/service";
 
+let btn= document.querySelector('input');
+let txt= document.querySelector('p');
 
+btn?.addEventListener('click',()=> {
+    new Service().listCo().then(listCo => {
+         for (const col of listCo ) {
+             if (txt) {
+                 txt.innerHTML = (listCo.map(c => c.nom + " " + c.prenom)).join(' ')
+             }
+         }
+    })
 
+})
